58b13127594975a4beadfa01b4c296c320bf836b
adakamahesh <adakamahesh@gmail.com>
2024-11-06 07:45:37 +0530

    add redux toolkit

--- src/components/NavBarPanel.js
+++ src/components/NavBarPanel.js
@@ -1,9 +1,11 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const NavBar = () =>{
+    const cartProducts = useSelector(state => state.cart);
     return(
     <Navbar className="bg-body-tertiary" >
       <Container fluid>
@@ -14,7 +16,7 @@ const NavBar = () =>{
           <Navbar.Toggle/>
           <Navbar.Collapse className='justify-content-end'>
             <Navbar.Text>
-            <Nav.Link to="/cart" as={Link}>My Bag 0</Nav.Link>
+            <Nav.Link to="/cart" as={Link}>My Bag {cartProducts.length}</Nav.Link>
             </Navbar.Text>
           </Navbar.Collapse>
       </Container>
